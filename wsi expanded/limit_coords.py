9cbd73e07cfd5581333cace82f2219ffb17e8946
nazari02105 <X.LordAliX@yahoo.com>
2023-02-24 09:33:06 +0330

    limit coords file now support more number of files

--- wsi expanded/limit_coords.py
+++ wsi expanded/limit_coords.py
@@ -3,10 +3,17 @@ tumor_valid_patch_path = "../PATCHES_TUMOR_VALID/list.txt"
 normal_train_patch_path = "../PATCHES_NORMAL_TRAIN/list.txt"
 normal_valid_patch_path = "../PATCHES_NORMAL_VALID/list.txt"
 
-tumor_train_list = ["Tumor_001"]
-tumor_valid_list = ["Tumor_101"]
-normal_train_list = ["Normal_001"]
-normal_valid_list = ["Normal_141"]
+tumor_train_list_server = ["tumor_001", "tumor_002", "tumor_003", "tumor_004", "tumor_005", "tumor_006", "tumor_007", "tumor_008"]
+tumor_valid_list_server = ["tumor_101", "tumor_102"]
+normal_train_list_server = ["normal_001", "normal_002", "normal_003", "normal_004", "normal_005", "normal_006", "normal_007", "normal_008"]
+normal_valid_list_server = ["normal_141", "normal_142"]
+all_list_server = tumor_train_list_server + tumor_valid_list_server + normal_train_list_server + normal_valid_list_server
+
+tumor_train_list = ["tumor_001"]
+tumor_valid_list = ["tumor_101"]
+normal_train_list = ["normal_001"]
+normal_valid_list = ["normal_141"]
+all_list = tumor_train_list + tumor_valid_list + normal_train_list + normal_valid_list
 
 
 def limit_coords_def(wsi_names, patch_path):
@@ -22,7 +29,7 @@ def limit_coords_def(wsi_names, patch_path):
     f.close()
 
 
-limit_coords_def(tumor_train_list, tumor_train_patch_path)
-limit_coords_def(tumor_valid_list, tumor_valid_patch_path)
-limit_coords_def(normal_train_list, normal_train_patch_path)
-limit_coords_def(normal_valid_list, normal_valid_patch_path)
+limit_coords_def(all_list, tumor_train_patch_path)
+limit_coords_def(all_list, tumor_valid_patch_path)
+limit_coords_def(all_list, normal_train_patch_path)
+limit_coords_def(all_list, normal_valid_patch_path)
